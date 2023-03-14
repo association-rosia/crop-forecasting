23cd13cbdc55d9cff6c969cb49aebb0249ad3bb2
BaptisteUrgell <baptiste.u@gmail.com>
2023-03-14 18:03:04 +0100

    Modif constant file

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,11 @@
-FOLDER = 'adaptative_factor_2'
+FOLDER = 'augment_10_5'
+
+BANDS = ['red', 'green', 'blue', 'rededge1', 'rededge2', 'rededge3', 'nir', 'swir']
+VI = ['ndvi', 'savi', 'evi', 'rep','osavi','rdvi','mtvi1','lswi']
+
 M_COLUMNS = ['tempmax', 'tempmin', 'temp', 'dew', 'humidity', 'precip', 'precipprob', 'precipcover', 'windspeed', 'winddir', 
              'sealevelpressure', 'cloudcover', 'solarradiation', 'solarenergy', 'uvindex', 'moonphase', 'solarexposure']
-
 S_COLUMNS = ['ndvi', 'savi', 'evi', 'rep', 'osavi', 'rdvi', 'mtvi1', 'lswi']
 G_COLUMNS = ['Field size (ha)', 'Rice Crop Intensity(D=Double, T=Triple)']
-LABEL = 'Rice Yield (kg/ha)'
+
+TARGET = 'Rice Yield (kg/ha)'
